22f4a11edd7d5b37b82205a6d561cb39ee335b1d
juneverbena <syexn098@gmail.com>
2021-01-09 16:14:49 +0900

    first sy

---
+++ mbti/app/src/main/java/com/yubdduk/mbti/ReceipeAdapter.java
@@ -0,0 +1,20 @@
+package com.yubdduk.mbti;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+/*
+public class ReceipeAdapter extends RecyclerView.Adapter<ReceipeAdapter.ReceipeViewHolder>{
+    private ReceipeInfo mData;
+
+    public static class ReceipeViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView textview;
+
+        //public ReceipeViewHolder(){
+
+        //}
+    }
+}
+*/
