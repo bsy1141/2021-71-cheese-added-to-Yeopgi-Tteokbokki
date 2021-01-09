20c589a3c3cefb5103e255d6bbca01d631b793e9
bsy1141 <bsy1141@naver.com>
2021-01-09 22:58:23 +0900

    hungry nation willnotcook yesterday add

---
+++ mbti/app/src/main/java/com/yubdduk/mbti/NationActivity.java
@@ -0,0 +1,13 @@
+package com.yubdduk.mbti;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nation);
+    }
+
+}
---
+++ mbti/app/src/main/java/com/yubdduk/mbti/WillNotCook_ResultActivity.java
@@ -0,0 +1,11 @@
+package com.yubdduk.mbti;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WillNotCook_ResultActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.);
+    }
+}
